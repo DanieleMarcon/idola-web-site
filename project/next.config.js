a3b0ac51ad3d16d5a5abd12544f46484725c8062
Daniele Marcon <morn83@hotmail.it>
2025-01-16 17:39:41 +0100

    Update V-1.1

--- project/next.config.js
+++ project/next.config.js
@@ -4,15 +4,7 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { 
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
